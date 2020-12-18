1215e57a4e55877497ef6807988fb7910b9648ed
Lorenzo Fiamingo <l99fiamingo@gmail.com>
2020-12-18 12:27:23 +0100

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HomeKitADK"
+)
+
+
+//.target(name: <#T##String#>, dependencies: <#T##[Target.Dependency]#>, path: <#T##String?#>, exclude: <#T##[String]#>, sources: <#T##[String]?#>, resources: <#T##[Resource]?#>, publicHeadersPath: <#T##String?#>, cSettings: <#T##[CSetting]?#>, cxxSettings: <#T##[CXXSetting]?#>, swiftSettings: <#T##[SwiftSetting]?#>, linkerSettings: <#T##[LinkerSetting]?#>)
